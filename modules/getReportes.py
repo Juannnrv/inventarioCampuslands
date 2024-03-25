e4eb7ecc478253ee746538981fb1125ea3524192
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-25 15:02:55 -0500

    reporte de listar activos y activos por categoria LISTO

--- modules/getReportes.py
+++ modules/getReportes.py
@@ -1,12 +1,48 @@
 import os
 import json
 import re
+import requests
+from tabulate import tabulate
 
-#def listarActivos():
-#http://url/activos
 
-#def listarActivosCategoria():
-#http://url/activos?categoria={categoria}
+def listarActivos():
+    peticion = requests.get('http://154.38.171.54:5502/activos')
+    data = peticion.json()
+    return data
+
+def CategoriasActivo():
+    while True:
+        os.system('clear')
+        print('''
+                                ---CATEGORIAS DE ACTIVOS---
+
+                                    1. Equipo de computo
+                                    2. Electrodomestico
+                                    3. Juego
+                                    0. Regresar al menú de reportes
+    ''')
+        try:
+            opcion = input('\nSeleccione una de las opciones => ')
+            if re.match(r'[0-9]+$', opcion):
+                opcion = int(opcion)
+                if opcion >= 0 and opcion <= 3:
+                    if opcion == 1:
+                        peticion = requests.get('http://154.38.171.54:5502/activos?idCategoria=1')
+                        data = peticion.json()
+                        return data
+                    elif opcion == 2:
+                        peticion = requests.get('http://154.38.171.54:5502/activos?idCategoria=2')
+                        data = peticion.json()
+                        return data
+                    elif opcion == 3:
+                        peticion = requests.get('http://154.38.171.54:5502/activos?idCategoria=3')
+                        data = peticion.json()
+                        return data 
+                    elif opcion == 0:
+                        break
+
+        except KeyboardInterrupt:
+            print('\nRegresando al menú de reportes...')
 
 #def listarActivosDebaja():
 #http://url/activos?estado=2
@@ -15,16 +51,6 @@ import re
 
 
 #def listarHistorialMovimientoActivos():
-
-
-
-
-
-
-
-
-
-
 
 def menuReportes():
     while True:
@@ -37,7 +63,7 @@ def menuReportes():
                     3. Listar activos por dados de baja por daño
                     4. Listar activos y asignación
                     5. Listar historial de mov. de activo
-                    6. Regresar al menú principal
+                    6. Regresar al menú  principal
 ''')
         try:
             opcion = input('\nSeleccione una de las opciones => ')
@@ -45,9 +71,30 @@ def menuReportes():
                 opcion = int(opcion)
                 if opcion >= 0 and opcion <= 6:
                     if opcion == 1:
-                        listarActivos()
+                        print(tabulate(listarActivos(), headers='keys', tablefmt='fancy grid'))
+                        input('Presiona la tecla Enter para continuar...')
                     elif opcion == 2:
-                        listarActivosCategoria()
+                        while True:
+                            print(tabulate(CategoriasActivo(), headers='keys', tablefmt='fancy grid'))
+                            print("""
+                            
+                            
+                                        ¿Deseas observar las demas categorias?
+                                
+                                                        1. Si
+                                                        2. No
+""")
+                            opcion = input('\nSeleccione una de las opciones => ')
+                            try:
+                                if re.match(r'[0-9]+$', opcion):
+                                    opcion = int(opcion)
+                                    if opcion >= 1 and opcion <= 2:
+                                        if opcion == 1:
+                                            print(tabulate(CategoriasActivo(), headers='keys', tablefmt='fancy grid'))
+                                        elif opcion == 2:
+                                            break  
+                            except KeyboardInterrupt:
+                                print()
                     elif opcion == 3:
                         listarActivosDeBaja()
                     elif opcion == 4:
@@ -59,4 +106,5 @@ def menuReportes():
 
         except KeyboardInterrupt:
             print('\nSeleccione una de las opciones => ')
+            
 

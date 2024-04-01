c1adf10bb7f02adbb825496bf39cf5f95581f8dd
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-31 20:56:07 -0500

    Dar de baja y Enviar a garantia LISTO

--- modules/getMovimientosActivos.py
+++ modules/getMovimientosActivos.py
@@ -54,7 +54,8 @@ def retornarActivo(id):
                                         res = peticion.json()
                                         if 'Mensaje' in res:
                                             print(res['Mensaje'])
-                                        print('\Activo retornado satisfactoriamente...')
+                                        print('\nActivo retornado satisfactoriamente...')
+                                        print()
                                         return [res]
                                     else:
                                         raise Exception('\nID de la persona no encontrado...')
@@ -72,13 +73,12 @@ def retornarActivo(id):
                         break
         else:
             print('\nEl activo ya se encuentra en estado no asignado...')
-            input('\nPresiona la tecla Enter para poder continuar...')
 
-def dardebaja():
+def dardebaja(id):
     # Obtener la información del activo con el ID proporcionado
     data = gA.ActivosId(id)
     if data:
-        if data[0]["idEstado"] == "0": # NO SE PUEDE DAR DE BAJA UN ACTIVO QUE ESTE ASIGNADO
+        if data[0]["idEstado"] != "1": # NO SE PUEDE DAR DE BAJA UN ACTIVO QUE ESTE ASIGNADO (idEstado == "1")
             while True:
                     os.system('clear')
                     print("""
@@ -114,7 +114,8 @@ def dardebaja():
                                         res = peticion.json()
                                         if 'Mensaje' in res:
                                             print(res['Mensaje'])
-                                        print('\Activo retornado satisfactoriamente...')
+                                        print('\nActivo dado de baja satisfactoriamente...')
+                                        print()
                                         return [res]
                                     else:
                                         raise Exception('\nID de la persona no encontrado...')
@@ -132,9 +133,68 @@ def dardebaja():
                         break
         else:
             print('\nEl activo se encuentra asignado...')
-            input('\nPresiona la tecla Enter para poder continuar...')
 
 
+
+def enviaraGarantia(id):
+    # Obtener la información del activo con el ID proporcionado
+    data = gA.ActivosId(id)
+    if data:
+        if data[0]["idEstado"] != "3": 
+            while True:
+                    os.system('clear')
+                    print("""
+                    A continuación retornarás un activo en SISTEMA G&C DE INVENTARIO CAMPUSLANDS
+                    
+                                            ¿Deseas continuar?
+                        
+                                                1. Si
+                                                2. No
+            """)
+                    opcion = input('\nSeleccione una de las opciones => ')
+                    try:
+                        if re.match(r'[0-9]+$', opcion):
+                            opcion = int(opcion)
+                            if opcion >= 1 and opcion <= 2:
+                                if opcion == 1:
+                                    
+                                    añadirhistorial = {}
+                                    idRespMov = input('\nIngrese el ID de la persona responsable del movimiento => ')
+                                    if gP.PersonasID(idRespMov):
+                                        añadirhistorial["NroId"] = shortuuid.random(length=4)
+                                        añadirhistorial["fecha"] = fecha_formateada
+                                        añadirhistorial["tipoMov"] = "3" 
+                                        añadirhistorial["idRespMov"] = idRespMov
+                                        diccsolo = data[0]
+                                        historialActivos = diccsolo["historialActivos"]
+                                        historialActivos.append(añadirhistorial)
+                                        data[0]['idEstado'] = "3"
+                                        data[0]['asignaciones'] = []
+
+                                        # Enviar la solicitud PUT para actualizar el activo
+                                        peticion = requests.put(f"http://154.38.171.54:5502/activos/{id}", data=json.dumps(data[0]).encode("UTF-8"))
+                                        res = peticion.json()
+                                        if 'Mensaje' in res:
+                                            print(res['Mensaje'])
+                                        print('\nActivo enviado a garantía satisfactoriamente...')
+                                        print()
+                                        return [res]
+                                    else:
+                                        raise Exception('\nID de la persona no encontrado...')
+
+                                elif opcion == 2:
+                                    break
+                            else:
+                                raise Exception('---> El dato ingresado debe ser 1 o 2')
+                        else:
+                            raise Exception('---> El dato ingresado debe ser 1 o 2') 
+                        
+                    except Exception as error:
+                        print('\n---ERROR---')
+                        print(error)
+                        break
+        else:
+            print('\nEl activo ya se encuentra en garantía...')
 
 
 
@@ -167,12 +227,18 @@ def menuMovimientoActivos():
                         print()
                         print(tabulate(retornarActivo(idretorno), headers='keys', tablefmt='fancy_grid'))
                         input('\nPresiona la tecla Enter para poder continuar...')
-                    # elif opcion == 2:
-                    #     deleteActivos()
+                    elif opcion == 2:
+                        idbaja = input('\nIngresa el ID del activo que desea dar de baja => ')
+                        print()
+                        print(tabulate(dardebaja(idbaja), headers='keys', tablefmt='fancy_grid'))
+                        input('\nPresiona la tecla Enter para poder continuar...')
                     # elif opcion == 3:
                     #     editActivos()
-                    # elif opcion == 4:
-                    #     searchActivos()
+                    elif opcion == 4:
+                        idgarantia = input('\nIngresa el ID del activo que desea enviar a garantía => ')
+                        print()
+                        print(tabulate(enviaraGarantia(idgarantia), headers='keys', tablefmt='fancy_grid'))
+                        input('\nPresiona la tecla Enter para poder continuar...')
                     elif opcion == 5:
                         break
                     

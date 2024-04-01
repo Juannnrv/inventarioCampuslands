6875057b4b650b1337326fa1ec5c95208febec03
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-31 19:54:48 -0500

    Error de logica arreglado

--- modules/getMovimientosActivos.py
+++ modules/getMovimientosActivos.py
@@ -18,7 +18,7 @@ def retornarActivo(id):
     # Obtener la información del activo con el ID proporcionado
     data = gA.ActivosId(id)
     if data:
-        if data[0]["idEstado"] == "0":
+        if data[0]["idEstado"] != "0":
             while True:
                     os.system('clear')
                     print("""
@@ -41,7 +41,67 @@ def retornarActivo(id):
                                     if gP.PersonasID(idRespMov):
                                         añadirhistorial["NroId"] = shortuuid.random(length=4)
                                         añadirhistorial["fecha"] = fecha_formateada
-                                        añadirhistorial["tipoMov"] = "0"
+                                        añadirhistorial["tipoMov"] = "0"  # Creamos el idTipo 0 para referirnos que el Activo fue RETORNADO
+                                        añadirhistorial["idRespMov"] = idRespMov
+                                        diccsolo = data[0]
+                                        historialActivos = diccsolo["historialActivos"]
+                                        historialActivos.append(añadirhistorial)
+                                        data[0]['idEstado'] = "0"
+                                        data[0]['asignaciones'] = []
+
+                                        # Enviar la solicitud PUT para actualizar el activo
+                                        peticion = requests.put(f"http://154.38.171.54:5502/activos/{id}", data=json.dumps(data[0]).encode("UTF-8"))
+                                        res = peticion.json()
+                                        if 'Mensaje' in res:
+                                            print(res['Mensaje'])
+                                        print('\Activo retornado satisfactoriamente...')
+                                        return [res]
+                                    else:
+                                        raise Exception('\nID de la persona no encontrado...')
+
+                                elif opcion == 2:
+                                    break
+                            else:
+                                raise Exception('---> El dato ingresado debe ser 1 o 2')
+                        else:
+                            raise Exception('---> El dato ingresado debe ser 1 o 2') 
+                        
+                    except Exception as error:
+                        print('\n---ERROR---')
+                        print(error)
+                        break
+        else:
+            print('\nEl activo ya se encuentra en estado no asignado...')
+            input('\nPresiona la tecla Enter para poder continuar...')
+
+def dardebaja():
+    # Obtener la información del activo con el ID proporcionado
+    data = gA.ActivosId(id)
+    if data:
+        if data[0]["idEstado"] == "0": # NO SE PUEDE DAR DE BAJA UN ACTIVO QUE ESTE ASIGNADO
+            while True:
+                    os.system('clear')
+                    print("""
+                    A continuación retornarás un activo en SISTEMA G&C DE INVENTARIO CAMPUSLANDS
+                    
+                                            ¿Deseas continuar?
+                        
+                                                1. Si
+                                                2. No
+            """)
+                    opcion = input('\nSeleccione una de las opciones => ')
+                    try:
+                        if re.match(r'[0-9]+$', opcion):
+                            opcion = int(opcion)
+                            if opcion >= 1 and opcion <= 2:
+                                if opcion == 1:
+                                    
+                                    añadirhistorial = {}
+                                    idRespMov = input('\nIngrese el ID de la persona responsable del movimiento => ')
+                                    if gP.PersonasID(idRespMov):
+                                        añadirhistorial["NroId"] = shortuuid.random(length=4)
+                                        añadirhistorial["fecha"] = fecha_formateada
+                                        añadirhistorial["tipoMov"] = "2" 
                                         añadirhistorial["idRespMov"] = idRespMov
                                         diccsolo = data[0]
                                         historialActivos = diccsolo["historialActivos"]
@@ -73,8 +133,6 @@ def retornarActivo(id):
         else:
             print('\nEl activo se encuentra asignado...')
             input('\nPresiona la tecla Enter para poder continuar...')
-
-
 
 
 

a712d09657311cb4bb5680d65d44581b41ed3e4a
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-31 19:37:03 -0500

    Retorno de activo LISTO

--- modules/getMovimientosActivos.py
+++ modules/getMovimientosActivos.py
@@ -1,7 +1,78 @@
 import os
 import json
 import re
+import requests
+import modules.getActivos as gA
+import modules.getPersonal as gP
+import datetime
+import shortuuid
+import random
+from tabulate import tabulate
 
+# Obtener la fecha actual
+fecha_actual = datetime.date.today()
+
+fecha_formateada = fecha_actual.strftime("%y-%m-%d")
+
+def retornarActivo(id):
+    # Obtener la información del activo con el ID proporcionado
+    data = gA.ActivosId(id)
+    if data:
+        if data[0]["idEstado"] == "0":
+            while True:
+                    os.system('clear')
+                    print("""
+                    A continuación retornarás un activo en SISTEMA G&C DE INVENTARIO CAMPUSLANDS
+                    
+                                            ¿Deseas continuar?
+                        
+                                                1. Si
+                                                2. No
+            """)
+                    opcion = input('\nSeleccione una de las opciones => ')
+                    try:
+                        if re.match(r'[0-9]+$', opcion):
+                            opcion = int(opcion)
+                            if opcion >= 1 and opcion <= 2:
+                                if opcion == 1:
+                                    
+                                    añadirhistorial = {}
+                                    idRespMov = input('\nIngrese el ID de la persona responsable del movimiento => ')
+                                    if gP.PersonasID(idRespMov):
+                                        añadirhistorial["NroId"] = shortuuid.random(length=4)
+                                        añadirhistorial["fecha"] = fecha_formateada
+                                        añadirhistorial["tipoMov"] = "0"
+                                        añadirhistorial["idRespMov"] = idRespMov
+                                        diccsolo = data[0]
+                                        historialActivos = diccsolo["historialActivos"]
+                                        historialActivos.append(añadirhistorial)
+                                        data[0]['idEstado'] = "0"
+                                        data[0]['asignaciones'] = []
+
+                                        # Enviar la solicitud PUT para actualizar el activo
+                                        peticion = requests.put(f"http://154.38.171.54:5502/activos/{id}", data=json.dumps(data[0]).encode("UTF-8"))
+                                        res = peticion.json()
+                                        if 'Mensaje' in res:
+                                            print(res['Mensaje'])
+                                        print('\Activo retornado satisfactoriamente...')
+                                        return [res]
+                                    else:
+                                        raise Exception('\nID de la persona no encontrado...')
+
+                                elif opcion == 2:
+                                    break
+                            else:
+                                raise Exception('---> El dato ingresado debe ser 1 o 2')
+                        else:
+                            raise Exception('---> El dato ingresado debe ser 1 o 2') 
+                        
+                    except Exception as error:
+                        print('\n---ERROR---')
+                        print(error)
+                        break
+        else:
+            print('\nEl activo se encuentra asignado...')
+            input('\nPresiona la tecla Enter para poder continuar...')
 
 
 
@@ -34,13 +105,16 @@ def menuMovimientoActivos():
                 opcion = int(opcion)
                 if opcion >= 0 and opcion <= 5:
                     if opcion == 1:
-                        postActivos()
-                    elif opcion == 2:
-                        deleteActivos()
-                    elif opcion == 3:
-                        editActivos()
-                    elif opcion == 4:
-                        searchActivos()
+                        idretorno = input('\nIngresa el ID del activo que desea retornar => ')
+                        print()
+                        print(tabulate(retornarActivo(idretorno), headers='keys', tablefmt='fancy_grid'))
+                        input('\nPresiona la tecla Enter para poder continuar...')
+                    # elif opcion == 2:
+                    #     deleteActivos()
+                    # elif opcion == 3:
+                    #     editActivos()
+                    # elif opcion == 4:
+                    #     searchActivos()
                     elif opcion == 5:
                         break
                     

49833839a67942104cd4568003ee02ed5bf62fd6
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-04-01 07:12:57 -0500

    Movimientos de activos LISTO

--- modules/getMovimientosActivos.py
+++ modules/getMovimientosActivos.py
@@ -27,7 +27,7 @@ def retornarActivo(id):
                                             ¿Deseas continuar?
                         
                                                 1. ✅ Si
-                                                2. No
+                                                2. ❌ No
             """)
                     opcion = input('\nSeleccione una de las opciones => ')
                     try:
@@ -87,7 +87,7 @@ def dardebaja(id):
                                             ¿Deseas continuar?
                         
                                                 1. ✅ Si
-                                                2. No
+                                                2. ❌ No
             """)
                     opcion = input('\nSeleccione una de las opciones => ')
                     try:
@@ -134,7 +134,93 @@ def dardebaja(id):
         else:
             print('\nEl activo se encuentra asignado...')
 
+def cambiarAsignacion(id):
+    # Obtener la información del activo con el ID proporcionado
+    data = gA.ActivosId(id)
+    if data:
+        if data[0]["idEstado"] == "1": 
+            while True:
+                    os.system('clear')
+                    print("""
+                    A continuación enviaras un activo a garantía en SISTEMA G&C DE INVENTARIO CAMPUSLANDS
+                    
+                                            ¿Deseas continuar?
+                        
+                                                1. ✅ Si
+                                                2. ❌ No
+            """)
+                    opcion = input('\nSeleccione una de las opciones => ')
+                    try:
+                        if re.match(r'[0-9]+$', opcion):
+                            opcion = int(opcion)
+                            if opcion >= 1 and opcion <= 2:
+                                if opcion == 1:
+                                    
+                                    print("""
+                                        Movimientos
 
+                                    0. Retornado  
+                                    1. Asignado
+                                    2. Dado de baja
+                                    3. En garantía
+                                    4. Reasignado
+                                """)
+                                    opcion = input('\nSeleccione una de las opciones => ')
+                                    try:
+                                        if re.match(r'[0-9]+$', opcion):
+                                            opcion = int(opcion)
+                                            if opcion >= 0 and opcion <= 4:
+                                                str(opcion)
+                                            else:
+                                                raise Exception('---> El dato ingresado debe ser un dígito del 0 al 4')
+                                        else:
+                                            raise Exception('---> El dato ingresado debe ser 1 o 2') 
+                                        
+                                    except Exception as error:
+                                        print('\n---ERROR---')
+                                        print(error)
+                                        break
+                                    
+                                    añadirhistorial = {}
+                                    idRespMov = input('\nIngrese el ID de la persona responsable del movimiento => ')
+                                    if gP.PersonasID(idRespMov):
+                                        añadirhistorial["NroId"] = shortuuid.random(length=4)
+                                        añadirhistorial["fecha"] = fecha_formateada
+                                        añadirhistorial["tipoMov"] = str(opcion)
+                                        añadirhistorial["idRespMov"] = idRespMov
+                                        diccsolo = data[0]
+                                        historialActivos = diccsolo["historialActivos"]
+                                        historialActivos.append(añadirhistorial)
+                                        if opcion == 4:
+                                            data[0]['idEstado'] = "1"
+                                        else:
+                                            data[0]['idEstado'] = str(opcion)
+                                        data[0]['asignaciones'] = []
+
+                                        # Enviar la solicitud PUT para actualizar el activo
+                                        peticion = requests.put(f"http://154.38.171.54:5502/activos/{id}", data=json.dumps(data[0]).encode("UTF-8"))
+                                        res = peticion.json()
+                                        if 'Mensaje' in res:
+                                            print(res['Mensaje'])
+                                        print('\nEl Activo fue cambiado de asignación satisfactoriamente...')
+                                        print()
+                                        return [res]
+                                    else:
+                                        raise Exception('\nID de la persona no encontrado...')
+
+                                elif opcion == 2:
+                                    break
+                            else:
+                                raise Exception('---> El dato ingresado debe ser 1 o 2')
+                        else:
+                            raise Exception('---> El dato ingresado debe ser 1 o 2') 
+                        
+                    except Exception as error:
+                        print('\n---ERROR---')
+                        print(error)
+                        break
+        else:
+            print('\nEl activo no cuenta con una asignación prevía...')
 
 def enviaraGarantia(id):
     # Obtener la información del activo con el ID proporcionado
@@ -144,12 +230,12 @@ def enviaraGarantia(id):
             while True:
                     os.system('clear')
                     print("""
-                    A continuación retornarás un activo en SISTEMA G&C DE INVENTARIO CAMPUSLANDS
+                    A continuación enviaras un activo a garantía en SISTEMA G&C DE INVENTARIO CAMPUSLANDS
                     
                                             ¿Deseas continuar?
                         
                                                 1. ✅ Si
-                                                2. No
+                                                2. ❌ No
             """)
                     opcion = input('\nSeleccione una de las opciones => ')
                     try:
@@ -239,8 +325,11 @@ def menuMovimientoActivos():
                         print()
                         print(tabulate(dardebaja(idbaja), headers='keys', tablefmt='fancy_grid'))
                         input('\nPresiona la tecla Enter para poder continuar...')
-                    # elif opcion == 3:
-                    #     editActivos()
+                    elif opcion == 3:
+                        idasignacion = input('\nIngresa el ID del activo el cual desea cambiar de asignación => ')
+                        print()
+                        print(tabulate(cambiarAsignacion(idasignacion), headers='keys', tablefmt='fancy_grid'))
+                        input('\nPresiona la tecla Enter para poder continuar...')
                     elif opcion == 4:
                         idgarantia = input('\nIngresa el ID del activo que desea enviar a garantía => ')
                         print()
